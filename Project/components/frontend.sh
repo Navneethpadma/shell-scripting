b6ec33da53c61d51203ebf54c74ac29ae5df0591
Navneethpadma <navneeth778@gmail.com>
2021-06-22 12:31:09 -0700

    Project started

--- Project/components/frontend.sh
+++ Project/components/frontend.sh
@@ -1,24 +1,43 @@
 #!/bin/bash
 
-echo -e "\e[1;35m [INFORMATION] Installing Nginx\e[0m"
 echo "------------------------------------------------------------"
+echo -e "\e[1;35m [INFORMATION] Installing Nginx\e[0m"
 echo "------------------------------------------------------------"
 
 yum install nginx -y
 if [ $? -ne 0 ]; then
+  echo "------------------------------------------------------------"
   echo -e "\e[1;35m [Error] Failure Installing Nginx\e[0m"
   exit 2
 else
-  echo -e "\e[1;40m [succ] Nginx installation is sucessful\e[0m"
+  echo "------------------------------------------------------------"
+  echo -e "\e[1;40m [succ] Nginx installation is successful\e[0m"
 fi
+
 echo "------------------------------------------------------------"
+echo "------------------------------------------------------------"
+echo -e "\e[1;35m [INFORMATION] Installing Nginx\e[0m"
 echo "------------------------------------------------------------"
 
-exit
+
+echo "------------------------------------------------------------"
+echo -e "\e[1;35m [INFORMATION] Download frontend component\e[0m"
+echo "------------------------------------------------------------"
 
 
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+if [ $? -ne 0 ]; then
+  echo "------------------------------------------------------------"
+  echo -e "\e[1;35m [Error] download failure\e[0m"
+  exit 2
+else
+  echo "------------------------------------------------------------"
+  echo -e "\e[1;40m [succ] Download successful is successful\e[0m"
+fi
+
+exit
+
 cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip

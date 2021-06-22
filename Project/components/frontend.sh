9ebb9649b65e72a782bb3a45a1978ec7b540d617
Navneethpadma <navneeth778@gmail.com>
2021-06-22 12:10:32 -0700

    Project starte d

--- Project/components/frontend.sh
+++ Project/components/frontend.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 echo -e "\e[1;35m [INFORMATION] Installing Nginx\e[0m"
-yum install nginx
+echo"------------------------------------------------------------"
+echo"------------------------------------------------------------"
+
+yum install nginx -y
 if [ $? -ne 0 ]; then
   echo -e "\e[1;35m [Error] Failure Installing Nginx\e[0m"
   exit 2
 else
   echo -e "\e[1;31m Nginx installation is sucessful\e[0m"
 fi
+echo"------------------------------------------------------------"
+echo"------------------------------------------------------------"
+
 
 systemctl enable nginx
 systemctl start nginx

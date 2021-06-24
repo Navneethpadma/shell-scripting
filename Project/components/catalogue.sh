20a139965d8b2b5f46ea9899cf948a73e1ea6061
Navneethpadma <navneeth778@gmail.com>
2021-06-24 14:49:21 -0700

    Project started

--- Project/components/catalogue.sh
+++ Project/components/catalogue.sh
@@ -1,2 +1,36 @@
 #!/bin/bash
-echo installing catalogue
+source components/common.sh
+components=catalogue
+
+print "Installing Node J.s"
+yum install nodejs make gcc-c++ -y
+STAT $? "Node JS Installation successful"
+
+print "Adding User"
+useradd roboshop
+STAT $? "successfully added user"
+
+#print "Roboshop login"
+#su -robohop
+#STAT $? "I am a roboshop user now"
+
+print "Download catalogue zip files"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+STAT $? "Successful downloading zip files"
+
+print "Setting us Catalogue files"
+cd /home/roboshop
+rm -rf catalogue && unzip /tmp/catalogue.zip && mv catalogue-main catalogue
+STAT $? "Extraction successful"
+
+print "Node js dependencies"
+cd /home/roboshop/catalogue
+npm install
+STAT $? "downloading dependencies successful"
+
+
+
+#mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+# systemctl daemon-reload
+# systemctl start catalogue
+# systemctl enable catalogue

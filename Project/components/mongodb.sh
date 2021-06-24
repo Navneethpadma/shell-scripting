e532fe575368e9ec9d85561e4a5e588bbadfd3b3
Navneethpadma <navneeth778@gmail.com>
2021-06-24 13:25:37 -0700

    Project started

--- Project/components/mongodb.sh
+++ Project/components/mongodb.sh
@@ -8,5 +8,22 @@ baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
-STAT $? "Mongodb installation is successful
-"
+STAT $? "Mongodb installation is successful"
+
+print "Installing mongodb files"
+yum install -y mongodb-org
+STAT $? "Mongodb installation is successful"
+#print "Starting Mongodb"
+#systemctl enable mongod
+#systemctl start mongod
+#STAT $? "Mongodb enabling is successful"
+
+#/etc/mongod.conf
+# systemctl restart mongod
+# curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+
+# cd /tmp
+# unzip mongodb.zip
+# cd mongodb-main
+# mongo < catalogue.js
+# mongo < users.js

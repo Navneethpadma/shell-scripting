81644904bbe76e5f5d4258f4b569aaf6da4c52f8
Navneethpadma <navneeth778@gmail.com>
2021-06-24 17:21:25 -0700

    Project started

--- Instance-create/create.sh
+++ Instance-create/create.sh
@@ -7,4 +7,4 @@ if [ -z "${COMPONENT}" ]; then
   echo "Component name input is needed"
   exit 1
 fi
-aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} --tag-specifications "ResourceType=instance, Tags=[{Key=Name, Value=${COMPONENT}}]" | jq
+aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=${COMPONENT}}]" | jq

c5a88a5af553167c84d2fe2abb485df49b670df8
Navneethpadma <navneeth778@gmail.com>
2021-06-29 14:57:42 -0700

    Project started

--- Project/components/redis.sh
+++ Project/components/redis.sh
@@ -9,4 +9,18 @@ STAT $? "Setting up redis repo"
 
 print "Install redis"
 yum install redis -y
-STAT $? " Installing Redis  "
+STAT $? " Installing Redis"
+
+print "Update redis configuration file"
+sed -i -e '/^bind 0.0.0.0' /etc/redis.conf
+STAT $? "Updating configuration files of redis"
+
+print "Start Redis service"
+systemctl enable redis
+systemctl restart redis
+STAT $? "Starting Redis Service"
+
+
+
+
+

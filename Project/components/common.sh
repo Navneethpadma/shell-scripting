8d8c8fa3265b013205f5e73eb7a62b3f1d956955
Navneethpadma <navneeth778@gmail.com>
2021-06-29 18:30:07 -0700

    Project started

--- Project/components/common.sh
+++ Project/components/common.sh
@@ -78,7 +78,7 @@ INSTALL_NODEJS_DEPENDENCIES()
 setup_service() {
   print "Setup SystemD Service for ${COMPONENT}"
   mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
-  sed -i  -e 's/MONGO_DNSNAME/mongodb.thenavops.com/' -e 's/MONGO_ENDPOINT/mongodb.thenavops.com/' -e 's/REDIS_ENDPOINT/mongodb.thenavops.com/' /etc/systemd/system/${COMPONENT}.service
+  sed -i  -e 's/MONGO_DNSNAME/mongodb.thenavops.com/' -e 's/MONGO_ENDPOINT/mongodb.thenavops.com/' -e 's/REDIS_ENDPOINT/redis.thenavops.com/' -e 's/CATALOGUE-ENDPOINT/mongodb.thenavops.com/' /etc/systemd/system/${COMPONENT}.service
   systemctl daemon-reload && systemctl start ${COMPONENT} && systemctl enable ${COMPONENT}
   STAT $? "Starting ${COMPONENT} Service"
 
